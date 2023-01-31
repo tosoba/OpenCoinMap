f964742179b8cba99da89f37dadd057f53a21ec1
Tomasz Osoba <tomasz.osoba@knowit.eu>
2023-01-31 15:10:01 +0100

    using new lib versions in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,12 +37,12 @@ android {
 }
 
 dependencies {
-  implementation("androidx.core:core-ktx:1.9.0")
-  implementation("androidx.appcompat:appcompat:1.6.0")
-  implementation("com.google.android.material:material:1.8.0")
-  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-  implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-  implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.constraintlayout)
+  implementation(libs.androidx.navigation.fragment)
+  implementation(libs.androidx.navigation.ui)
+  implementation(libs.google.material)
 
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
