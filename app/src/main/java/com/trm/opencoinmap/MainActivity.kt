2c5a6fb86c88299dc837d98bc7dbb8665ea24d21
tosoba <therealmerengue@gmail.com>
2023-03-26 18:30:29 +0200

    remove content_main

--- app/src/main/java/com/trm/opencoinmap/MainActivity.kt
+++ app/src/main/java/com/trm/opencoinmap/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -70,7 +71,10 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
   }
 
   private fun initNavigation() {
-    val navController = findNavController(R.id.nav_host_fragment_content_main)
+    val navHostFragment =
+      supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main)
+        as NavHostFragment
+    val navController = navHostFragment.navController
     appBarConfiguration = AppBarConfiguration(navController.graph)
     setupActionBarWithNavController(navController, appBarConfiguration)
   }
