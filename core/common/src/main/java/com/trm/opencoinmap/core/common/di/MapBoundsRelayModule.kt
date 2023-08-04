8d1d6110fdfb5d8d04e49a619bf404a4eccb5eb3
Tomasz Osoba <tomasz.osoba@knowit.eu>
2023-08-04 16:23:53 +0200

    venue list update after initial full sync does not require map move anymore

--- core/common/src/main/java/com/trm/opencoinmap/core/common/di/MapBoundsRelayModule.kt
+++ core/common/src/main/java/com/trm/opencoinmap/core/common/di/MapBoundsRelayModule.kt
@@ -1,6 +1,6 @@
 package com.trm.opencoinmap.core.common.di
 
-import com.jakewharton.rxrelay3.PublishRelay
+import com.jakewharton.rxrelay3.BehaviorRelay
 import com.trm.opencoinmap.core.domain.model.MapBounds
 import com.trm.opencoinmap.core.domain.usecase.ReceiveMapBoundsUseCase
 import com.trm.opencoinmap.core.domain.usecase.SendMapBoundsUseCase
@@ -12,7 +12,7 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object MapBoundsRelayModule {
-  private val relay = PublishRelay.create<MapBounds>()
+  private val relay = BehaviorRelay.create<MapBounds>()
 
   @Provides fun sendMapBoundsUseCase() = SendMapBoundsUseCase(relay::accept)
   @Provides fun receiveMapBoundsUseCase() = ReceiveMapBoundsUseCase { relay }
