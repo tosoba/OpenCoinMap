4ea86c119e14c3d43673c9d0ccbf7fc944872d64
tosoba <therealmerengue@gmail.com>
2023-09-23 13:19:13 +0200

    MaterialCardView background for about dialog

--- feature/about/src/main/kotlin/com/trm/opencoinmap/feature/about/AboutFragment.kt
+++ feature/about/src/main/kotlin/com/trm/opencoinmap/feature/about/AboutFragment.kt
@@ -1,6 +1,8 @@
 package com.trm.opencoinmap.feature.about
 
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
 import android.view.Window
@@ -11,11 +13,11 @@ import com.trm.opencoinmap.feature.about.databinding.FragmentAboutBinding
 class AboutFragment : DialogFragment(R.layout.fragment_about) {
   private val binding by viewBinding(FragmentAboutBinding::bind)
 
-  override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-    val dialog = super.onCreateDialog(savedInstanceState)
-    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-    return dialog
-  }
+  override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+    super.onCreateDialog(savedInstanceState).apply {
+      requestWindowFeature(Window.FEATURE_NO_TITLE)
+      window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
