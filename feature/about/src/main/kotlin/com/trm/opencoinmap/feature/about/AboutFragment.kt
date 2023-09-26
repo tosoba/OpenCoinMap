7ee95c49741ee0d71c66fb8a619e5b4b03d1ff19
tosoba <therealmerengue@gmail.com>
2023-09-26 22:50:24 +0200

    add copyable crypto addresses

--- feature/about/src/main/kotlin/com/trm/opencoinmap/feature/about/AboutFragment.kt
+++ feature/about/src/main/kotlin/com/trm/opencoinmap/feature/about/AboutFragment.kt
@@ -2,6 +2,8 @@ package com.trm.opencoinmap.feature.about
 
 import android.app.Dialog
 import android.content.ActivityNotFoundException
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
@@ -10,6 +12,8 @@ import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.DialogFragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.trm.opencoinmap.core.common.R as commonR
@@ -32,6 +36,23 @@ class AboutFragment : DialogFragment(R.layout.fragment_about) {
   private fun FragmentAboutBinding.initViews() {
     aboutOsmCreditLayer.setOnClickListener { goToUrlInBrowser("https://www.openstreetmap.org/") }
     aboutCoinMapCreditLayer.setOnClickListener { goToUrlInBrowser("https://coinmap.org/") }
+
+    aboutBitcoinSupportMeLabel.isSelected = true
+    aboutEthereumSupportMeLabel.isSelected = true
+    aboutXrpSupportMeLabel.isSelected = true
+
+    aboutBitcoinSupportMeLayer.setOnClickListener {
+      copyToClipboard(addressRes = R.string.bitcoin_address)
+    }
+    aboutEthereumSupportMeLayer.setOnClickListener {
+      copyToClipboard(addressRes = R.string.ethereum_address)
+    }
+    aboutXrpSupportMeLayer.setOnClickListener { copyToClipboard(addressRes = R.string.xrp_address) }
+  }
+
+  private fun copyToClipboard(@StringRes addressRes: Int) {
+    getSystemService(requireContext(), ClipboardManager::class.java)
+      ?.setPrimaryClip(ClipData.newPlainText(null, requireContext().getString(addressRes)))
   }
 
   private fun goToUrlInBrowser(url: String) {
