6b279892fa11153e434ad7825c0ed8fdf1ab4f56
tosoba <therealmerengue@gmail.com>
2023-09-26 22:01:04 +0200

    simplified compose theme

--- app/src/main/kotlin/com/trm/opencoinmap/theme/Theme.kt
+++ app/src/main/kotlin/com/trm/opencoinmap/theme/Theme.kt
@@ -1,75 +1,41 @@
 package com.trm.opencoinmap.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-val LightColorScheme =
-  lightColorScheme(
-    primary = light_primary,
-    onPrimary = light_onPrimary,
-    primaryContainer = light_primaryContainer,
-    onPrimaryContainer = light_onPrimaryContainer,
-    secondary = light_secondary,
-    onSecondary = light_onSecondary,
-    secondaryContainer = light_secondaryContainer,
-    onSecondaryContainer = light_onSecondaryContainer,
-    tertiary = light_tertiary,
-    onTertiary = light_onTertiary,
-    tertiaryContainer = light_tertiaryContainer,
-    onTertiaryContainer = light_onTertiaryContainer,
-    error = light_error,
-    onError = light_onError,
-    errorContainer = light_errorContainer,
-    onErrorContainer = light_onErrorContainer,
-    outline = light_outline,
-    background = light_background,
-    onBackground = light_onBackground,
-    surface = light_surface,
-    onSurface = light_onSurface,
-    surfaceVariant = light_surfaceVariant,
-    onSurfaceVariant = light_onSurfaceVariant,
-    inverseSurface = light_inverseSurface,
-    inverseOnSurface = light_inverseOnSurface,
-    inversePrimary = light_inversePrimary,
-    surfaceTint = light_surfaceTint,
-    outlineVariant = light_outlineVariant,
-    scrim = light_scrim,
+private val darkColorScheme: ColorScheme =
+  darkColorScheme(
+    primary = purple80,
+    secondary = purpleGrey80,
+    tertiary = pink80,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black,
+    onTertiary = Color.Black,
   )
 
-val DarkColorScheme =
-  darkColorScheme(
-    primary = dark_primary,
-    onPrimary = dark_onPrimary,
-    primaryContainer = dark_primaryContainer,
-    onPrimaryContainer = dark_onPrimaryContainer,
-    secondary = dark_secondary,
-    onSecondary = dark_onSecondary,
-    secondaryContainer = dark_secondaryContainer,
-    onSecondaryContainer = dark_onSecondaryContainer,
-    tertiary = dark_tertiary,
-    onTertiary = dark_onTertiary,
-    tertiaryContainer = dark_tertiaryContainer,
-    onTertiaryContainer = dark_onTertiaryContainer,
-    error = dark_error,
-    onError = dark_onError,
-    errorContainer = dark_errorContainer,
-    onErrorContainer = dark_onErrorContainer,
-    outline = dark_outline,
-    background = dark_background,
-    onBackground = dark_onBackground,
-    surface = dark_surface,
-    onSurface = dark_onSurface,
-    surfaceVariant = dark_surfaceVariant,
-    onSurfaceVariant = dark_onSurfaceVariant,
-    inverseSurface = dark_inverseSurface,
-    inverseOnSurface = dark_inverseOnSurface,
-    inversePrimary = dark_inversePrimary,
-    surfaceTint = dark_surfaceTint,
-    outlineVariant = dark_outlineVariant,
-    scrim = dark_scrim,
+private val lightColorScheme: ColorScheme =
+  lightColorScheme(
+    primary = purple40,
+    secondary = purpleGrey40,
+    tertiary = pink40,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
   )
 
 @Composable
@@ -78,7 +44,7 @@ fun OpenCoinMapComposeTheme(
   content: @Composable () -> Unit
 ) {
   MaterialTheme(
-    colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme,
+    colorScheme = if (darkTheme) darkColorScheme else lightColorScheme,
     typography = Typography,
     content = content
   )
