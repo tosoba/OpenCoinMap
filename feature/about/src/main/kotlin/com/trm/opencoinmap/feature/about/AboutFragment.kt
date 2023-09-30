6beaee3def03d0c99e51eeb90e7aaa73bb616eb5
tosoba <therealmerengue@gmail.com>
2023-09-30 12:06:22 +0200

    show address copied below android 13

--- feature/about/src/main/kotlin/com/trm/opencoinmap/feature/about/AboutFragment.kt
+++ feature/about/src/main/kotlin/com/trm/opencoinmap/feature/about/AboutFragment.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -78,6 +79,11 @@ class AboutFragment : DialogFragment(R.layout.fragment_about) {
   private fun copyToClipboard(@StringRes addressRes: Int) {
     getSystemService(requireContext(), ClipboardManager::class.java)
       ?.setPrimaryClip(ClipData.newPlainText(null, requireContext().getString(addressRes)))
+      ?.also {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2) {
+          Toast.makeText(requireContext(), R.string.address_copied, Toast.LENGTH_SHORT).show()
+        }
+      }
   }
 
   private fun goToUrlInBrowser(url: String) {
