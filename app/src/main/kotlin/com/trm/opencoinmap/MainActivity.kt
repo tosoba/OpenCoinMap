1ebc7b2df88335a5423a66c845503537051dc996
tosoba <therealmerengue@gmail.com>
2023-12-14 21:30:56 +0100

    remove setDecorFitsSystemWindows + add top margin to webView

--- app/src/main/kotlin/com/trm/opencoinmap/MainActivity.kt
+++ app/src/main/kotlin/com/trm/opencoinmap/MainActivity.kt
@@ -1,14 +1,6 @@
 package com.trm.opencoinmap
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-    super.onCreate(savedInstanceState)
-  }
-}
+@AndroidEntryPoint class MainActivity : AppCompatActivity(R.layout.activity_main)
